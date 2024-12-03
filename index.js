7f26bd89309377b542e2f012bf4587d5622d3e59
Kritz <tanakrit.promwungkwa@gmail.com>
2024-12-03 16:58:43 +0700

    added app check

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // The Cloud Functions for Firebase SDK to create Cloud Functions and triggers.
 const { logger } = require("firebase-functions");
-const { onRequest } = require("firebase-functions/v2/https");
+const { onRequest, onCall } = require("firebase-functions/v2/https");
 const { onDocumentCreated } = require("firebase-functions/v2/firestore");
 
 // The Firebase Admin SDK to access Firestore.
@@ -11,61 +11,64 @@ initializeApp();
 
 // Take the text parameter passed to this HTTP endpoint and insert it into
 // Firestore under the path /messages/:documentId/original
-exports.addTodo = onRequest(async (req, res) => {
-  try {
-    const body = req.body;
-    const result = await getFirestore().collection("todo").add(body);
+exports.addTodoV2 = onCall(
+  {
+    enforceAppCheck: true,
+  },
+  async (req, res) => {
+    console.log(req);
+    try {
+      const body = req.body;
+      const result = await getFirestore().collection("todo").add(body);
 
-    res.status(201).json({ id: result.id });
-  } catch (err) {
-    res.status(500).json(err.message);
-  }
-});
-
-exports.getTodo = onRequest(async (req, res) => {
-  try {
-    const col = await getFirestore().collection("todo").get();
-    const result = [];
-    for (const doc of col.docs) {
-      const data = doc.data();
-      data["id"] = doc.id;
-      result.push(data);
+      res.status(201).json({ id: result.id });
+    } catch (err) {
+      res.status(500).json(err.message);
     }
-
-    res.status(200).json(result);
-  } catch (err) {
-    res.status(500).json(err.message);
   }
-});
+);
 
-exports.updateTodo = onRequest(async (req, res) => {
-  try {
-    const docId = req.params["0"];
-    const body = req.body;
-    const result = await getFirestore()
-      .doc("todo/" + docId)
-      .update(body);
+exports.getTodoV2 = onCall(
+  {
+    enforceAppCheck: true,
+  },
+  async (req, res) => {
+    try {
+      const col = await getFirestore().collection("todo").get();
+      const result = [];
+      for (const doc of col.docs) {
+        const data = doc.data();
+        data["id"] = doc.id;
+        result.push(data);
+      }
 
-    res.status(200).json(result);
-  } catch (err) {
-    res.status(500).json(err.message);
+      res.status(200).json(result);
+    } catch (err) {
+      res.status(500).json(err.message);
+    }
   }
-});
+);
 
-// // Listens for new messages added to /messages/:documentId/original
-// // and saves an uppercased version of the message
-// // to /messages/:documentId/uppercase
-// exports.makeuppercase = onDocumentCreated("/messages/{documentId}", (event) => {
-//   // Grab the current value of what was written to Firestore.
-//   const original = event.data.data().original;
+exports.updateTodoV2 = onCall(
+  {
+    enforceAppCheck: true,
+  },
+  async (req, res) => {
+    try {
+      const docId = req.params["0"];
+      const body = req.body;
+      const result = await getFirestore()
+        .doc("todo/" + docId)
+        .update(body);
 
-//   // Access the parameter `{documentId}` with `event.params`
-//   logger.log("Uppercasing", event.params.documentId, original);
+      res.status(200).json(result);
+    } catch (err) {
+      res.status(500).json(err.message);
+    }
+  }
+);
 
-//   const uppercase = original.toUpperCase();
-
-//   // You must return a Promise when performing
-//   // asynchronous tasks inside a function
+// asynchronous tasks inside a function
 //   // such as writing to Firestore.
 //   // Setting an 'uppercase' field in Firestore document returns a Promise.
 //   return event.data.ref.set({uppercase}, {merge: true});
